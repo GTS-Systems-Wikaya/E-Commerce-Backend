5b7dbfa3a90457ac6cbfb612390f549fade6ab5a
ahmed-bhh <ahmedbelhajhassen22@gmail.com>
2024-07-26 11:57:04 +0100

    projet vierge

---
+++ GTS-system/src/main/java/projet/gtssystem/GtsSystemApplication.java
@@ -0,0 +1,13 @@
+package projet.gtssystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtsSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtsSystemApplication.class, args);
+	}
+
+}
